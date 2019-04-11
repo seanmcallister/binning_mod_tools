a4c8df558fce082d05dba2dc9f891cb326d09bb6
Sean McAllister <mcallis2@gmail.com>
2019-04-11 06:51:33 -0400

    Speeds up concoct_pull_contigs.pl

--- scripts/concoct_pull_contigs.pl
+++ scripts/concoct_pull_contigs.pl
@@ -4,7 +4,6 @@ use Getopt::Std;
 
 # - - - - - H E A D E R - - - - - - - - - - - - - - - - -
 #Goals of script:
-#Strangely, CONCOCT doesn't pull out bins after binning is completed. This should fix that!
 #Take the cluster output file from CONCOCT (out_clustering_gt2000.csv) and use to pull bins from the original fasta file.
 
 # - - - - - U S E R    V A R I A B L E S - - - - - - - -
@@ -40,17 +39,16 @@ foreach my $out (@clust_data)
         open($outbase, ">$options{o}"."_bin_".$data[1].".fasta");        
     }
 
-foreach my $i (sort keys %Sequences)
-	{	foreach my $m (@clust_data)
-		{       chomp($m);
-                        my @data = split(',', $m);
-                        my $outnum = "bin".$data[1];
-			if($Sequences{$i}{'SHORT_HEAD'} eq $data[0])
-                            { print $outnum ">".$Sequences{$i}{'HEAD'}."\n".$Sequences{$i}{'gappy-ntseq'}."\n";
-                              shift(@clust_data);
-                            }
-		}
+foreach my $m (@clust_data)
+	{	chomp($m);
+                my @data = split(',', $m);
+                my $outnum = "bin".$data[1];
+		if(exists $Sequences{$data[0]})
+                	{ print $outnum ">".$Sequences{$data[0]}{'HEAD'}."\n".$Sequences{$data[0]}{'gappy-ntseq'}."\n";
+                          #shift(@clust_data);
+                        }
 	}
+	
 
 foreach my $out (@clust_copy)
     {   chomp($out);
@@ -72,23 +70,23 @@ sub FASTAread
 	open(IN, "<$infile") or die "\n\nNADA $infile you FOOL!!!\n\n";
 	my @DATA = <IN>; close(IN); shift(@DATA);	
 	# 2. Parse sequence data . . . . . . . . . . . . .
-	my $unid = $filenumber.10001;                           # string to generate unique ids
+	#my $unid = $filenumber.10001;                           # string to generate unique ids
 	foreach my $entry (@DATA)
 	{	my @data = split('\n', $entry);
 		my $seq = '';
 		foreach my $i (1..$#data)
 		{	$seq .= $data[$i];  }
 		$seq =~ s/>//;
-		$Sequences{$unid}{'HEAD'}    = $data[0];       # store header
+		$Sequences{$data[0]}{'HEAD'}    = $data[0];       # store header
 		my @shorthead = split(' ', $data[0]);
-		$Sequences{$unid}{'SHORT_HEAD'} = $shorthead[0];
-		$Sequences{$unid}{'gappy-ntseq'}   = uc($seq);       # store aligned sequence
-		$Sequences{$unid}{'SIZE'}    = length($seq);   # store length
+		$Sequences{$data[0]}{'SHORT_HEAD'} = $shorthead[0];
+		$Sequences{$data[0]}{'gappy-ntseq'}   = uc($seq);       # store aligned sequence
+		$Sequences{$data[0]}{'SIZE'}    = length($seq);   # store length
 		$seq =~ s/\.//;
                 $seq =~ s/\-//;
-                $Sequences{$unid}{'degapped-ntseq'} = uc($seq);     # store degapped sequence
-                $Sequences{$unid}{'filenumber'} = $filenumber;
-                $unid += 1;
+                $Sequences{$data[0]}{'degapped-ntseq'} = uc($seq);     # store degapped sequence
+                $Sequences{$data[0]}{'filenumber'} = $filenumber;
+                #$unid += 1;
 	}
 	$/="\n";
 }
